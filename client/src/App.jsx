53314f509fc73bbcbc23c1c2afa9dab5dd86e951
dpeyupkaya <dpeyupkaya@gmail.com>
2025-03-26 01:41:30 +0300

    Update package.json to upgrade Vite to version 6.2.3; remove unused CartProvider and MainLayout components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,16 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import React from 'react';
-import CartProvider, { CartContext } from './context/CartProvider';
-import MainLayout from './layout/MainLayout';
+
 function App() {
   return (
     <BrowserRouter>
-    <CartProvider>
-      <MainLayout>
+ 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-      </Routes>
-      </MainLayout>
-    </CartProvider>
+     </Routes>
     
     </BrowserRouter>
   );
